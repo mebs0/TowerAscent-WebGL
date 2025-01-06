adb6d63cb88220d7bab6fe0a1c5b07be8a7eecd9
mebs0 <mebs.ishaq@hotmail.com>
2025-01-06 05:37:56 +0000

    build
    build

--- speech.js
+++
@@ -1,9 +0,0 @@
-// This function is called from Unity to trigger speech synthesis
-function SpeakText(text) {
-    if ('speechSynthesis' in window) {
-        var msg = new SpeechSynthesisUtterance(text);
-        window.speechSynthesis.speak(msg);
-    } else {
-        console.log("Speech synthesis not supported.");
-    }
-}
